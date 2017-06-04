d068a7a4a134b063dc6b90a90391ac478e719b21
Carlos Perez <solracias@gmail.com>
2017-06-04 16:53:47 -0700

    asdf

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,16 +24,35 @@ connection.query("SELECT * FROM products", function (error, results, fields){
 
 connection.end();
 
-inquirer.prompt([
-    {
-        name: "id",
-        message: "which ID are you looking to buy? "
-    },
-    {
-        name: "quantity",
-        message: "how many would you like to buy? "
-    }
-]).then(function(answers){
-    console.log(answers.id);
-    console.log(answers.quantity);
-})
+function run(){
+    inquirer.prompt([
+        {
+            name: "id",
+            message: "which ID are you looking to buy? "
+        },
+        {
+            name: "quantity",
+            message: "how many would you like to buy? "
+        }
+    ]).then(function(answers){
+        results.stock_quantity -= answers.stock_quantity;
+        // console.log(answers.id);
+        // console.log(answers.quantity);
+        
+        connection.query("UPDATE products SET stock_quantity="+results.stock_quantity + "WHERE id=" + answers.id);
+
+        connection.query("SELECT * FROM products", function (error, results, fields){
+            console.log(fields[0].name,"  "+ fields[1].name,"  "+  fields[2].name,"  "+  fields[3].name,"  "+  fields[4].name);
+            if (error) {
+                throw error;
+            }
+
+            for (var i = 0; i < results.length; i++){
+                console.log(results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity);
+            }
+            
+        });
+    })
+}
+
+setTimeout(run, 500);
