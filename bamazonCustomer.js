a7b34ca22a1e8ed1f10c335b1ecc96fed4d7cf05
Carlos Perez <solracias@gmail.com>
2017-06-11 10:43:00 -0700

    data is now displayed in an organized table. Easy to read.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,10 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host : "localhost",
+    host : "127.0.0.1",
+    port: 3306,
     user : "root",
     password : "root",
     database : "bamazon_db"
@@ -11,15 +13,16 @@ var connection = mysql.createConnection({
 connection.connect();
 
 connection.query("SELECT * FROM products", function (error, results, fields){
-    console.log(fields[0].name,"  "+ fields[1].name,"  "+  fields[2].name,"  "+  fields[3].name,"  "+  fields[4].name);
-    if (error) {
-        throw error;
-    }
+    var table = new Table({
+        head: [fields[0].name, fields[1].name, fields[2].name, fields[3].name, fields[4].name],
+        colWidths: [9, 22, 22, 9, 15]
+    });
 
+    // table.push([results[0].item_id, results[0].product_name, results[0].department_name, results[0].price, results[0].stock_quantity]);
     for (var i = 0; i < results.length; i++){
-        console.log(results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity);
+        table.push([results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
     }
-    
+    console.log(table.toString());
 });
 
 connection.end();
@@ -41,17 +44,17 @@ function run(){
         
         connection.query("UPDATE products SET stock_quantity="+results.stock_quantity + "WHERE id=" + answers.id);
 
-        connection.query("SELECT * FROM products", function (error, results, fields){
-            console.log(fields[0].name,"  "+ fields[1].name,"  "+  fields[2].name,"  "+  fields[3].name,"  "+  fields[4].name);
-            if (error) {
-                throw error;
-            }
+        // connection.query("SELECT * FROM products", function (error, results, fields){
+        //     console.log(fields[0].name,"  "+ fields[1].name,"  "+  fields[2].name,"  "+  fields[3].name,"  "+  fields[4].name);
+        //     if (error) {
+        //         throw error;
+        //     }
 
-            for (var i = 0; i < results.length; i++){
-                console.log(results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity);
-            }
+        //     for (var i = 0; i < results.length; i++){
+        //         console.log(results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity);
+        //     }
             
-        });
+        // });
     })
 }
 
